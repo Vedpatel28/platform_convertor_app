ed5afa395873d2d1d01c69a5f72b5a06f4abaafe
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-06-20 09:00:17 +0530

    VP | 20/6/23 | # Cupertino Setting Page Completed UI...

--- lib/views/Screens/cu_setting_page.dart
+++ lib/views/Screens/cu_setting_page.dart
@@ -32,7 +32,11 @@ class cu_setting_page extends StatelessWidget {
             SizedBox(height: s.height * 0.15),
             SizedBox(
               width: s.width * 0.9,
-              height: (Provider.of<settingpagecontroller>(context, listen: false).profileSwitch) ? s.height * 0.42 : s.height * 0.1,
+              height:
+                  (Provider.of<settingpagecontroller>(context, listen: false)
+                          .profileSwitch)
+                      ? s.height * 0.42
+                      : s.height * 0.1,
               child: Column(
                 children: [
                   SizedBox(height: s.height * 0.01),
@@ -59,21 +63,29 @@ class cu_setting_page extends StatelessWidget {
                             // Change Theme
                             TextSpan(
                               text: 'Update Profile Data',
-                              style: TextStyle(color: CupertinoColors.inactiveGray, fontSize: 16, fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  color: CupertinoColors.inactiveGray,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ],
                         ),
                       ),
                       const Spacer(),
                       CupertinoSwitch(
-                        value: Provider.of<settingpagecontroller>(context, listen: true).profileSwitch,
+                        value: Provider.of<settingpagecontroller>(context,
+                                listen: true)
+                            .profileSwitch,
                         onChanged: (value) {
-                          Provider.of<settingpagecontroller>(context, listen: false).Expandprofile();
+                          Provider.of<settingpagecontroller>(context,
+                                  listen: false)
+                              .Expandprofile();
                         },
                       ),
                     ],
                   ),
-                  (Provider.of<settingpagecontroller>(context, listen: false).profileSwitch)
+                  (Provider.of<settingpagecontroller>(context, listen: false)
+                          .profileSwitch)
                       ? SingleChildScrollView(
                           child: Column(
                             // crossAxisAlignment: CrossAxisAlignment.center,
@@ -99,13 +111,17 @@ class cu_setting_page extends StatelessWidget {
                                         width: s.width * 0.35,
                                         child: const CupertinoTextField(
                                           textInputAction: TextInputAction.next,
-                                          suffixMode: OverlayVisibilityMode.notEditing,
-                                          style: TextStyle(color: CupertinoColors.inactiveGray),
+                                          suffixMode:
+                                              OverlayVisibilityMode.notEditing,
+                                          style: TextStyle(
+                                              color:
+                                                  CupertinoColors.inactiveGray),
                                           textAlign: TextAlign.center,
                                           suffix: Text(
                                             "Enter your name...",
                                             style: TextStyle(
-                                              color: CupertinoColors.inactiveGray,
+                                              color:
+                                                  CupertinoColors.inactiveGray,
                                             ),
                                           ),
                                           decoration: BoxDecoration(),
@@ -121,13 +137,17 @@ class cu_setting_page extends StatelessWidget {
                                         width: s.width * 0.3,
                                         child: const CupertinoTextField(
                                           textInputAction: TextInputAction.next,
-                                          suffixMode: OverlayVisibilityMode.notEditing,
-                                          style: TextStyle(color: CupertinoColors.inactiveGray),
+                                          suffixMode:
+                                              OverlayVisibilityMode.notEditing,
+                                          style: TextStyle(
+                                              color:
+                                                  CupertinoColors.inactiveGray),
                                           textAlign: TextAlign.center,
                                           suffix: Text(
                                             "Enter your bio...",
                                             style: TextStyle(
-                                              color: CupertinoColors.inactiveGray,
+                                              color:
+                                                  CupertinoColors.inactiveGray,
                                             ),
                                           ),
                                           decoration: BoxDecoration(),
@@ -221,16 +241,23 @@ class cu_setting_page extends StatelessWidget {
                           // Change Theme
                           TextSpan(
                             text: 'Change Theme',
-                            style: TextStyle(color: CupertinoColors.inactiveGray, fontSize: 16, fontWeight: FontWeight.bold),
+                            style: TextStyle(
+                                color: CupertinoColors.inactiveGray,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
                     ),
                     const Spacer(),
                     CupertinoSwitch(
-                      value: Provider.of<settingpagecontroller>(context, listen: true).getTheme,
+                      value: Provider.of<settingpagecontroller>(context,
+                              listen: true)
+                          .getTheme,
                       onChanged: (value) {
-                        Provider.of<settingpagecontroller>(context, listen: false).changetheme();
+                        Provider.of<settingpagecontroller>(context,
+                                listen: false)
+                            .changetheme();
                       },
                     ),
                   ],
@@ -241,159 +268,160 @@ class cu_setting_page extends StatelessWidget {
         ),
       ),
     );
-
-    //   Column(
-    //   children: [
-    //     SizedBox(height: s.height * 0.02),
-    //     SizedBox(
-    //       width: s.width * 0.9,
-    //       height: (Provider.of<settingpagecontroller>(context, listen: false)
-    //               .profileSwitch)
-    //           ? s.height * 0.5
-    //           : s.height * 0.1,
-    //       child: Column(
-    //         children: [
-    //           SizedBox(height: s.height * 0.01),
-    //           Row(
-    //             children: [
-    //               SizedBox(width: s.width * 0.05),
-    //               const Icon(CupertinoIcons.person,
-    //                   color: CupertinoColors.activeGreen, size: 30),
-    //               SizedBox(width: s.width * 0.06),
-    //               // Profile / Update Profile Data
-    //               const Text.rich(
-    //                 TextSpan(
-    //                   children: [
-    //                     // Theme
-    //                     TextSpan(
-    //                       text: 'Profile \n',
-    //                       style: TextStyle(
-    //                         fontSize: 20,
-    //                         fontWeight: FontWeight.bold,
-    //                       ),
-    //                     ),
-    //                     // Change Theme
-    //                     TextSpan(
-    //                       text: 'Update Profile Data',
-    //                       style: TextStyle(
-    //                           color: CupertinoColors.inactiveGray,
-    //                           fontSize: 16,
-    //                           fontWeight: FontWeight.bold),
-    //                     ),
-    //                   ],
-    //                 ),
-    //               ),
-    //               const Spacer(),
-    //               CupertinoSwitch(
-    //                 value: Provider.of<settingpagecontroller>(context,
-    //                         listen: false)
-    //                     .profileSwitch,
-    //                 onChanged: (value) {
-    //                   Provider.of<settingpagecontroller>(context, listen: false)
-    //                       .Expandprofile();
-    //                 },
-    //               ),
-    //             ],
-    //           ),
-    //           (Provider.of<settingpagecontroller>(context, listen: false)
-    //                   .profileSwitch)
-    //               ? SingleChildScrollView(
-    //                   child: Column(
-    //                     crossAxisAlignment: CrossAxisAlignment.center,
-    //                     mainAxisAlignment: MainAxisAlignment.center,
-    //                     children: [
-    //                       SizedBox(height: s.height * 0.02),
-    //                       Container(
-    //                         height: s.height * 0.4,
-    //                         decoration: BoxDecoration(
-    //                           borderRadius: BorderRadius.circular(50),
-    //                         ),
-    //                         child: Icon(
-    //                           CupertinoIcons.photo_camera,
-    //                           size: s.height * 0.06,
-    //                         ),
-    //                       ),
-    //                       SizedBox(height: s.height * 0.02),
-    //                       Row(
-    //                         children: [
-    //                           SizedBox(width: s.width * 0.3),
-    //                           SizedBox(
-    //                             height: s.height * 0.06,
-    //                             width: s.width * 0.35,
-    //                             child: const CupertinoTextField(),
-    //                           ),
-    //                         ],
-    //                       ),
-    //                       SizedBox(height: s.height * 0.01),
-    //                       Row(
-    //                         children: [
-    //                           SizedBox(width: s.width * 0.32),
-    //                           SizedBox(
-    //                             height: s.height * 0.06,
-    //                             width: s.width * 0.35,
-    //                             child: const CupertinoTextField(),
-    //                           ),
-    //                         ],
-    //                       ),
-    //                       Row(
-    //                         children: [],
-    //                       ),
-    //                     ],
-    //                   ),
-    //                 )
-    //               : const SizedBox(),
-    //         ],
-    //       ),
-    //     ),
-    //     SingleChildScrollView(
-    //       child: SizedBox(
-    //         height: s.height * 0.1,
-    //         width: s.width * 0.9,
-    //         // color: Colors.grey,
-    //         child: Row(
-    //           children: [
-    //             SizedBox(width: s.width * 0.05),
-    //             const Icon(CupertinoIcons.brightness,
-    //                 color: CupertinoColors.inactiveGray, size: 30),
-    //             SizedBox(width: s.width * 0.06),
-    //             // Theme / Change Theme
-    //             const Text.rich(
-    //               TextSpan(
-    //                 children: [
-    //                   // Theme
-    //                   TextSpan(
-    //                     text: 'Theme\n',
-    //                     style: TextStyle(
-    //                       fontSize: 20,
-    //                       fontWeight: FontWeight.bold,
-    //                     ),
-    //                   ),
-    //                   // Change Theme
-    //                   TextSpan(
-    //                     text: 'Change Theme',
-    //                     style: TextStyle(
-    //                         color: CupertinoColors.inactiveGray,
-    //                         fontSize: 16,
-    //                         fontWeight: FontWeight.bold),
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //             const Spacer(),
-    //             CupertinoSwitch(
-    //               value:
-    //                   Provider.of<settingpagecontroller>(context, listen: false)
-    //                       .getTheme,
-    //               onChanged: (value) {
-    //                 Provider.of<settingpagecontroller>(context, listen: false)
-    //                     .changetheme();
-    //               },
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //   ],
-    // );
+    /*
+      Column(
+      children: [
+        SizedBox(height: s.height * 0.02),
+        SizedBox(
+          width: s.width * 0.9,
+          height: (Provider.of<settingpagecontroller>(context, listen: false)
+                  .profileSwitch)
+              ? s.height * 0.5
+              : s.height * 0.1,
+          child: Column(
+            children: [
+              SizedBox(height: s.height * 0.01),
+              Row(
+                children: [
+                  SizedBox(width: s.width * 0.05),
+                  const Icon(CupertinoIcons.person,
+                      color: CupertinoColors.activeGreen, size: 30),
+                  SizedBox(width: s.width * 0.06),
+                  // Profile / Update Profile Data
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        // Theme
+                        TextSpan(
+                          text: 'Profile \n',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        // Change Theme
+                        TextSpan(
+                          text: 'Update Profile Data',
+                          style: TextStyle(
+                              color: CupertinoColors.inactiveGray,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  CupertinoSwitch(
+                    value: Provider.of<settingpagecontroller>(context,
+                            listen: false)
+                        .profileSwitch,
+                    onChanged: (value) {
+                      Provider.of<settingpagecontroller>(context, listen: false)
+                          .Expandprofile();
+                    },
+                  ),
+                ],
+              ),
+              (Provider.of<settingpagecontroller>(context, listen: false)
+                      .profileSwitch)
+                  ? SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: s.height * 0.02),
+                          Container(
+                            height: s.height * 0.4,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: Icon(
+                              CupertinoIcons.photo_camera,
+                              size: s.height * 0.06,
+                            ),
+                          ),
+                          SizedBox(height: s.height * 0.02),
+                          Row(
+                            children: [
+                              SizedBox(width: s.width * 0.3),
+                              SizedBox(
+                                height: s.height * 0.06,
+                                width: s.width * 0.35,
+                                child: const CupertinoTextField(),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: s.height * 0.01),
+                          Row(
+                            children: [
+                              SizedBox(width: s.width * 0.32),
+                              SizedBox(
+                                height: s.height * 0.06,
+                                width: s.width * 0.35,
+                                child: const CupertinoTextField(),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [],
+                          ),
+                        ],
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          child: SizedBox(
+            height: s.height * 0.1,
+            width: s.width * 0.9,
+            // color: Colors.grey,
+            child: Row(
+              children: [
+                SizedBox(width: s.width * 0.05),
+                const Icon(CupertinoIcons.brightness,
+                    color: CupertinoColors.inactiveGray, size: 30),
+                SizedBox(width: s.width * 0.06),
+                // Theme / Change Theme
+                const Text.rich(
+                  TextSpan(
+                    children: [
+                      // Theme
+                      TextSpan(
+                        text: 'Theme\n',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Change Theme
+                      TextSpan(
+                        text: 'Change Theme',
+                        style: TextStyle(
+                            color: CupertinoColors.inactiveGray,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                CupertinoSwitch(
+                  value:
+                      Provider.of<settingpagecontroller>(context, listen: false)
+                          .getTheme,
+                  onChanged: (value) {
+                    Provider.of<settingpagecontroller>(context, listen: false)
+                        .changetheme();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+    */
   }
 }
